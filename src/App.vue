cebd5e30941228c461ebed58579b8beb8bf68d5a
noyanahmed96 <noyanahmed96@gmail.com>
2022-11-06 01:28:12 +0100

    Add vuetify theme

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,37 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <v-app>
+    <v-app-bar>
+      <v-container class="d-flex align-center py-0">
+        <v-app-bar-title class="pl-0">
+          <div class="d-flex align-center">
+            <v-avatar
+              rounded="0"
+              class="mr-3"
+              image="https://cdn.vuetifyjs.com/docs/images/logos/v.png"
+            />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it bro!" />
+            Vuetify 3
+          </div>
+        </v-app-bar-title>
+      </v-container>
+    </v-app-bar>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/graph">Graph</RouterLink>
-      </nav>
-    </div>
-  </header>
+    <v-main>
+      <RouterView />
+    </v-main>
 
-  <RouterView />
+    <v-footer>
+      <v-container class="text-overline d-flex align-center justify-space-between">
+        <div>
+          Copyright &copy; 2022 Vuetify, LLC
+        </div>
+
+        <v-icon icon="mdi-vuetify" size="x-large"></v-icon>
+      </v-container>
+    </v-footer>
+  </v-app>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<script>
+  export default { name: 'App' }
+</script>
